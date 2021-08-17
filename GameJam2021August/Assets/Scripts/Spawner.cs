7456ca7e7d5a9d5cfb0dcda2cdc9036aec5b87c3
DenDenDor <dendor333@gmail.com>
2021-08-17 17:51:13 +0300

    add

---
+++ GameJam2021August/Assets/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+   [SerializeField] private Color[] _colors;
+   [SerializeField] private Player _player;
+   private void Start() {
+      Player player = Instantiate(_player, transform.position, Quaternion.identity);
+      int random = Random.Range(0,_colors.Length);
+      player.Current_color = _colors[random];
+   }
+}
